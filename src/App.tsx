6b58fdd4c6949b5e0b90e812ee11ab9aeb5c261a
Kiran Neupane <burgeon.kiran@gmail.com>
2020-12-10 13:06:07 +0545

    removed bootstrap

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import './App.css';
 import TodoService from './modules/data/todo/TodoService';
 import TodoViewModelImpl from './modules/features/todo/view_model/TodoViewModelImpl';
 import TodoComponent from './modules/features/todo/view/TodoComponent';
